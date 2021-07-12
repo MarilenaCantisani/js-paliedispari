5c12cd579f1204551c721320b45932e8ab164013
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-07-12 14:47:43 +0200

    + Added structure exercise "Palidroma"

--- js/script-Palidroma.js
+++ js/script-Palidroma.js
@@ -1,3 +1,21 @@
 /* Palidroma:
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma */
+
+
+var userWord = prompt("Inserisci una parola");
+console.log("Parola utente: ", userWord);
+
+var userWordReverse = isPalidroma(userWord);
+
+if (userWord === userWordReverse) {
+    console.log("La parola inserita è palidroma!");
+} else {
+    console.log("La parola inserita non è palidroma!");
+}
+
+function isPalidroma(word) {
+    var wordReverse = word.split("").reverse().join("");
+    console.log("Parola invertita: ", wordReverse)
+    return wordReverse;
+}
